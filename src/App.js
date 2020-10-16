4c727909866ddc506ae2282cd5dbe9282b5a727b
James Land <hokie10@gmail.com>
2020-10-16 11:07:30 -0400

    Testing base url

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
 
 
   componentDidMount() {
-    const baseUrl = (process.env.REACT_APP_BACKEND_URL === undefined) ? 'http://localhost:8080' : process.env.test_URL
+    // const baseUrl = (process.env.REACT_APP_BACKEND_URL === undefined) ? 'http://localhost:8080' : process.env.REACT_APP_BACKEND_URL
+    const baseUrl = "https://example-app-jland-dev.apps.shared-na4.na4.openshift.opentlc.com/api/v1"
     fetch(baseUrl  + '/todos')
     .then(res => {
       return res.json()
